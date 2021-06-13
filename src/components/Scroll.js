e2b5cb2595e60bfcf59808fe51dbb3a0f9a8c8fb
swapnilg671 <swapnilg671@gmail.com>
2021-06-13 21:21:18 +0530

    class based App

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
 
-class Scroll extends Component {
-    render(props) {
-        console.log(this.props.children);
+const Scroll=(props) => {
         return (
             <div style={{overflowY:'scroll', border:'1.5px solid black',height:'600px'  }}>
-            {this.props.children}
+            {props.children}
             </div>
         )
-    }
-}
+    };
 
 export default Scroll
